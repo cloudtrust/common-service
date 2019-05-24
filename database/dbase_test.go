e92558f1d49297410e9ae523d71a5107299e222e
Francis PEROT <francis.perot@elca.ch>
2019-05-24 15:32:45 +0200

    fixup! Import flyway check feature from keycloak-bridge

--- database/dbase_test.go
+++ database/dbase_test.go
@@ -49,7 +49,7 @@ func TestConfigureDbDefault(t *testing.T) {
 	var envUser = "the env user"
 	var envPass = "the env password"
 
-	for _, suffix := range []string{"-host-port", "-username", "-password", "-database", "-protocol", "-max-open-conns", "-max-idle-conns", "-conn-max-lifetime"} {
+	for _, suffix := range []string{"-host-port", "-username", "-password", "-database", "-protocol", "-max-open-conns", "-max-idle-conns", "-conn-max-lifetime", "-migration", "-migration-version"} {
 		mockConf.EXPECT().SetDefault(prefix+suffix, gomock.Any()).Times(1)
 	}
 	mockConf.EXPECT().BindEnv(prefix+"-username", envUser).Times(1)
@@ -71,6 +71,8 @@ func TestGetDbConfig(t *testing.T) {
 	for _, suffix := range []string{"-max-open-conns", "-max-idle-conns", "-conn-max-lifetime"} {
 		mockConf.EXPECT().GetInt(prefix + suffix).Return(1).Times(1)
 	}
+	mockConf.EXPECT().GetBool(prefix + "-migration").Return(false).Times(1)
+	mockConf.EXPECT().GetString(prefix + "-migration-version").Return("1.0").Times(1)
 
 	var cfg = GetDbConfig(mockConf, prefix, false)
 	assert.Equal(t, "value-host-port", cfg.HostPort)
