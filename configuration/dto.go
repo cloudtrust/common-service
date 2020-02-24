820297dd5c118dc624fb653e0de68f9c0601e9c4
Francis PEROT <francis.perot@elca.ch>
2020-02-24 09:40:12 +0100

    [CLOUDTRUST-2298] Add api_self_account_editing_enabled and show_profile_tab to realm configuration

--- configuration/dto.go
+++ configuration/dto.go
@@ -1,15 +1,19 @@
 package configuration
 
-// RealmConfiguration struct
+import "encoding/json"
+
+// RealmConfiguration struct. APISelfAccountEditingEnabled replaces former field APISelfMailEditingEnabled
 type RealmConfiguration struct {
 	DefaultClientID                     *string   `json:"default_client_id,omitempty"`
 	DefaultRedirectURI                  *string   `json:"default_redirect_uri,omitempty"`
 	APISelfAuthenticatorDeletionEnabled *bool     `json:"api_self_authenticator_deletion_enabled,omitempty"`
 	APISelfPasswordChangeEnabled        *bool     `json:"api_self_password_change_enabled,omitempty"`
-	APISelfMailEditingEnabled           *bool     `json:"api_self_mail_editing_enabled,omitempty"`
+	DeprecatedAPISelfMailEditingEnabled *bool     `json:"api_self_mail_editing_enabled,omitempty"`
+	APISelfAccountEditingEnabled        *bool     `json:"api_self_account_editing_enabled,omitempty"`
 	APISelfAccountDeletionEnabled       *bool     `json:"api_self_account_deletion_enabled,omitempty"`
 	ShowAuthenticatorsTab               *bool     `json:"show_authenticators_tab,omitempty"`
 	ShowPasswordTab                     *bool     `json:"show_password_tab,omitempty"`
+	ShowProfileTab                      *bool     `json:"show_profile_tab,omitempty"`
 	ShowMailEditing                     *bool     `json:"show_mail_editing,omitempty"`
 	ShowAccountDeletionButton           *bool     `json:"show_account_deletion_button,omitempty"`
 	RegisterExecuteActions              *[]string `json:"register_execute_actions,omitempty"`
@@ -24,4 +28,18 @@ type Authorization struct {
 	Action          *string `json:"action"`
 	TargetRealmID   *string `json:"target_realm_id,omitempty"`
 	TargetGroupName *string `json:"target_group_name,omitempty"`
+}
+
+// NewRealmConfiguration returns the realm configuration from its JSON representation
+func NewRealmConfiguration(confJSON string) (RealmConfiguration, error) {
+	var conf RealmConfiguration
+	var err = json.Unmarshal([]byte(confJSON), &conf)
+	if err != nil {
+		return conf, err
+	}
+	if conf.DeprecatedAPISelfMailEditingEnabled != nil && conf.APISelfAccountEditingEnabled == nil {
+		conf.APISelfAccountEditingEnabled = conf.DeprecatedAPISelfMailEditingEnabled
+	}
+	conf.DeprecatedAPISelfMailEditingEnabled = nil
+	return conf, nil
 }
---
+++ configuration/dto_test.go
@@ -0,0 +1,24 @@
+package configuration
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewRealmConfiguration(t *testing.T) {
+	t.Run("Invalid JSON", func(t *testing.T) {
+		var _, err = NewRealmConfiguration(`{`)
+		assert.NotNil(t, err)
+	})
+	t.Run("Has deprecated field, new field not set", func(t *testing.T) {
+		var conf, _ = NewRealmConfiguration(`{"api_self_mail_editing_enabled":true}`)
+		assert.Nil(t, conf.DeprecatedAPISelfMailEditingEnabled)
+		assert.True(t, *conf.APISelfAccountEditingEnabled)
+	})
+	t.Run("Has deprecated field, new field already set", func(t *testing.T) {
+		var conf, _ = NewRealmConfiguration(`{"api_self_mail_editing_enabled":true, "api_self_account_editing_enabled":false}`)
+		assert.Nil(t, conf.DeprecatedAPISelfMailEditingEnabled)
+		assert.False(t, *conf.APISelfAccountEditingEnabled)
+	})
+}
