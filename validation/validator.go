c6423bc5b56d2ce02f648f36df40170ca6b60ae6
Francis PEROT <francis.perot@elca.ch>
2020-02-26 11:36:02 +0100

    [CLOUDTRUST-2314] Date validation enhancement

--- validation/validator.go
+++ validation/validator.go
@@ -16,6 +16,7 @@ type Validator interface {
 	ValidateParameterRegExp(prmName string, value *string, regExp string, mandatory bool) Validator
 	ValidateParameterPhoneNumber(prmName string, value *string, mandatory bool) Validator
 	ValidateParameterDate(prmName string, value *string, dateLayout string, mandatory bool) Validator
+	ValidateParameterDateMultipleLayout(prmName string, value *string, dateLayout []string, mandatory bool) Validator
 	ValidateParameterDateAfter(prmName string, value *string, dateLayout string, reference time.Time, mandatory bool) Validator
 	ValidateParameterDateBefore(prmName string, value *string, dateLayout string, reference time.Time, mandatory bool) Validator
 	ValidateParameterDateBetween(prmName string, value *string, dateLayout string, refAfter time.Time, refBetween time.Time, mandatory bool) Validator
@@ -87,28 +88,32 @@ func (v *successValidator) ValidateParameterPhoneNumber(prmName string, value *s
 }
 
 func (v *successValidator) ValidateParameterDate(prmName string, value *string, dateLayout string, mandatory bool) Validator {
+	return v.validateParameterDate(prmName, value, []string{dateLayout}, nil, nil, mandatory)
+}
+
+func (v *successValidator) ValidateParameterDateMultipleLayout(prmName string, value *string, dateLayout []string, mandatory bool) Validator {
 	return v.validateParameterDate(prmName, value, dateLayout, nil, nil, mandatory)
 }
 
 func (v *successValidator) ValidateParameterDateAfter(prmName string, value *string, dateLayout string, reference time.Time, mandatory bool) Validator {
-	return v.validateParameterDate(prmName, value, dateLayout, &reference, nil, mandatory)
+	return v.validateParameterDate(prmName, value, []string{dateLayout}, &reference, nil, mandatory)
 }
 
 func (v *successValidator) ValidateParameterDateBefore(prmName string, value *string, dateLayout string, reference time.Time, mandatory bool) Validator {
-	return v.validateParameterDate(prmName, value, dateLayout, nil, &reference, mandatory)
+	return v.validateParameterDate(prmName, value, []string{dateLayout}, nil, &reference, mandatory)
 }
 
 func (v *successValidator) ValidateParameterDateBetween(prmName string, value *string, dateLayout string, refAfter, refBefore time.Time, mandatory bool) Validator {
-	return v.validateParameterDate(prmName, value, dateLayout, &refAfter, &refBefore, mandatory)
+	return v.validateParameterDate(prmName, value, []string{dateLayout}, &refAfter, &refBefore, mandatory)
 }
 
-func (v *successValidator) validateParameterDate(prmName string, value *string, dateLayout string, referenceAfter, referenceBefore *time.Time, mandatory bool) Validator {
+func (v *successValidator) validateParameterDate(prmName string, value *string, dateLayout []string, referenceAfter, referenceBefore *time.Time, mandatory bool) Validator {
 	if value == nil {
 		if mandatory {
 			return &failedValidator{err: cerrors.CreateMissingParameterError(prmName)}
 		}
 	} else {
-		var date, err = time.Parse(dateLayout, *value)
+		var date, err = v.parseDate(dateLayout, *value)
 		if err != nil {
 			return &failedValidator{err: cerrors.CreateBadRequestError(cerrors.MsgErrInvalidParam + "." + prmName)}
 		}
@@ -120,6 +125,20 @@ func (v *successValidator) validateParameterDate(prmName string, value *string, 
 		}
 	}
 	return v
+}
+
+func (v *successValidator) parseDate(dateLayouts []string, value string) (time.Time, error) {
+	var resError error
+	for _, layout := range dateLayouts {
+		var date, err = time.Parse(layout, value)
+		if err == nil {
+			return date, err
+		}
+		if resError == nil {
+			resError = err
+		}
+	}
+	return time.Time{}, resError
 }
 
 func (v *successValidator) Status() error {
@@ -143,6 +162,10 @@ func (v *failedValidator) ValidateParameterPhoneNumber(prmName string, value *st
 }
 
 func (v *failedValidator) ValidateParameterDate(_ string, _ *string, _ string, _ bool) Validator {
+	return v
+}
+
+func (v *failedValidator) ValidateParameterDateMultipleLayout(prmName string, value *string, dateLayout []string, mandatory bool) Validator {
 	return v
 }
 
