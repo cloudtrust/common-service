d36ddcd0ea19dcfd7075d5b7c92b7b9f11671721
Francis PEROT <francis.perot@elca.ch>
2021-04-13 16:58:37 +0200

    [CLOUDTRUST-3113] Add a GLN field in RealmAdminConfiguration

--- configuration/dto.go
+++ configuration/dto.go
@@ -46,6 +46,7 @@ type RealmAdminConfiguration struct {
 	Theme               *string                   `json:"theme,omitempty"`
 	NeedVerifiedContact *bool                     `json:"need_verified_contact,omitempty"`
 	ConsentRequired     *bool                     `json:"consent_required,omitempty"`
+	GlnRequired         *bool                     `json:"gln,omitempty"`
 }
 
 // RealmAdminAccreditation struct
