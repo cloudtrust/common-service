b884e44c38dfaef9ac7ed714a4f64515f7059079
Bastien Beuchat <bastien.beuchat@elca.ch>
2023-09-25 18:01:23 +0200

    More robust tests

--- healthcheck/audit_test.go
+++ healthcheck/audit_test.go
@@ -17,45 +17,31 @@ func TestAuditEventsReporterChecker(t *testing.T) {
 	var mockAuditEventReporter = mock.NewAuditEventsReporterModule(mockCtrl)
 
 	t.Run("Success ", func(t *testing.T) {
-		var auditEventReporterChecker = newAuditEventsReporterChecker("alias", mockAuditEventReporter, 1*time.Second, 1*time.Second, log.NewNopLogger())
-		// first call
+		var auditEventReporterChecker = newAuditEventsReporterChecker("alias", mockAuditEventReporter, 10*time.Second, 10*time.Second, log.NewNopLogger())
+		internalChecker := auditEventReporterChecker.(*auditEventsReporterChecker)
 		mockAuditEventReporter.EXPECT().ReportEvent(gomock.Any(), gomock.Any()).Times(1)
 
-		var res = auditEventReporterChecker.CheckStatus()
-		assert.NotNil(t, res.Connection)
-		assert.Equal(t, "init", *res.Connection)
-
-		time.Sleep(2 * time.Second)
-		mockAuditEventReporter.EXPECT().ReportEvent(gomock.Any(), gomock.Any()).Times(1)
-
-		// usual success call
-		res = auditEventReporterChecker.CheckStatus()
+		internalChecker.updateStatus()
+		var res = internalChecker.response
 		assert.NotNil(t, res.Connection)
 		assert.Equal(t, "established", *res.Connection)
 
-		time.Sleep(500 * time.Millisecond)
-
-		// Get succes from the cache
-		res = auditEventReporterChecker.CheckStatus()
+		// Mock is configured to be called only once... A new call would let the test success as result is cached
+		internalChecker.updateStatus()
+		res = internalChecker.response
 		assert.NotNil(t, res.Connection)
 		assert.Equal(t, "established", *res.Connection)
 	})
 
 	t.Run("Failure ", func(t *testing.T) {
 		var auditEventReporterChecker = newAuditEventsReporterChecker("alias", mockAuditEventReporter, 1*time.Second, 10*time.Second, log.NewNopLogger())
+		internalChecker := auditEventReporterChecker.(*auditEventsReporterChecker)
 		mockAuditEventReporter.EXPECT().ReportEvent(gomock.Any(), gomock.Any()).Do(func(arg0 interface{}, arg1 interface{}) {
 			time.Sleep(2 * time.Second)
 		})
 
-		// first call
-		var res = auditEventReporterChecker.CheckStatus()
-		assert.NotNil(t, res.Connection)
-		assert.Equal(t, "init", *res.Connection)
-
-		time.Sleep(2 * time.Second)
-
-		// Call with down status
-		res = auditEventReporterChecker.CheckStatus()
+		internalChecker.updateStatus()
+		res := internalChecker.response
 		assert.NotNil(t, res.Message)
 		assert.Equal(t, "Events reporter timeout", *res.Message)
 	})
