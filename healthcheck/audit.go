7181a9fed56affb9d41942b95120135ebc102f1e
Bastien Beuchat <bastien.beuchat@elca.ch>
2023-10-06 09:59:46 +0200

    [CLOUDTRUST-5203] Add counter to liveness probe (#127)

--- healthcheck/audit.go
+++ healthcheck/audit.go
@@ -9,11 +9,12 @@ import (
 )
 
 type auditEventsReporterChecker struct {
-	alias    string
-	reporter events.AuditEventsReporterModule
-	timeout  time.Duration
-	response HealthStatus
-	logger   log.Logger
+	alias          string
+	reporter       events.AuditEventsReporterModule
+	timeout        time.Duration
+	response       HealthStatus
+	logger         log.Logger
+	failureCounter int
 }
 
 func newAuditEventsReporterChecker(alias string, reporter events.AuditEventsReporterModule, timeout time.Duration, cacheDuration time.Duration, logger log.Logger) BasicChecker {
@@ -22,11 +23,12 @@ func newAuditEventsReporterChecker(alias string, reporter events.AuditEventsRepo
 	response.connection("init")
 	response.stateUp()
 	return &auditEventsReporterChecker{
-		alias:    alias,
-		reporter: reporter,
-		timeout:  timeout,
-		response: response,
-		logger:   logger,
+		alias:          alias,
+		reporter:       reporter,
+		timeout:        timeout,
+		response:       response,
+		logger:         logger,
+		failureCounter: 0,
 	}
 }
 
@@ -50,9 +52,11 @@ func (a *auditEventsReporterChecker) updateStatus() {
 	case <-finished:
 		a.response.connection("established")
 		a.response.stateUp()
+		a.failureCounter = 0
 	case <-time.After(a.timeout):
 		a.response.stateDown("Events reporter timeout")
-		a.logger.Error(context.Background(), "msg", "Audit Events Reporter timeout to produce events", "timeout", a.timeout)
+		a.failureCounter++
+		a.logger.Error(context.Background(), "msg", "Audit Events Reporter timeout to produce events", "timeout", a.timeout, "failureCounter", a.failureCounter)
 	}
 
 	a.response.touch()
