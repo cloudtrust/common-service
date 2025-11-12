ffc9c527751c4e95ebae49461f3bd3efa3d4dced
Jonas Troeltsch <jonas.troeltsch@elca.ch>
2025-11-12 09:30:23 +0100

    [CLOUDTRUST-7206] add APISelfInitialPasswordDefinitionAllowed

--- configuration/dto.go
+++ configuration/dto.go
@@ -16,23 +16,24 @@ var (
 
 // RealmConfiguration struct. APISelfAccountEditingEnabled replaces former field APISelfMailEditingEnabled
 type RealmConfiguration struct {
-	DefaultClientID                     *string  `json:"default_client_id,omitempty"`
-	DefaultRedirectURI                  *string  `json:"default_redirect_uri,omitempty"`
-	APISelfAuthenticatorDeletionEnabled *bool    `json:"api_self_authenticator_deletion_enabled,omitempty"`
-	APISelfPasswordChangeEnabled        *bool    `json:"api_self_password_change_enabled,omitempty"`
-	DeprecatedAPISelfMailEditingEnabled *bool    `json:"api_self_mail_editing_enabled,omitempty"`
-	APISelfAccountEditingEnabled        *bool    `json:"api_self_account_editing_enabled,omitempty"`
-	APISelfAccountDeletionEnabled       *bool    `json:"api_self_account_deletion_enabled,omitempty"`
-	APISelfIDPLinksManagementEnabled    *bool    `json:"api_self_idplinks_management_enabled,omitempty"`
-	ShowAuthenticatorsTab               *bool    `json:"show_authenticators_tab,omitempty"`
-	ShowPasswordTab                     *bool    `json:"show_password_tab,omitempty"`
-	ShowProfileTab                      *bool    `json:"show_profile_tab,omitempty"`
-	ShowMailEditing                     *bool    `json:"show_mail_editing,omitempty"`
-	ShowAccountDeletionButton           *bool    `json:"show_account_deletion_button,omitempty"`
-	ShowIDPLinksTab                     *bool    `json:"show_idplinks_tab,omitempty"`
-	SelfServiceDefaultTab               *string  `json:"self_service_default_tab,omitempty"`
-	AllowedBackURL                      *string  `json:"allowed_back_url,omitempty"` // DEPRECATED
-	AllowedBackURLs                     []string `json:"allowed_back_urls,omitempty"`
+	DefaultClientID                         *string  `json:"default_client_id,omitempty"`
+	DefaultRedirectURI                      *string  `json:"default_redirect_uri,omitempty"`
+	APISelfAuthenticatorDeletionEnabled     *bool    `json:"api_self_authenticator_deletion_enabled,omitempty"`
+	APISelfPasswordChangeEnabled            *bool    `json:"api_self_password_change_enabled,omitempty"`
+	DeprecatedAPISelfMailEditingEnabled     *bool    `json:"api_self_mail_editing_enabled,omitempty"`
+	APISelfAccountEditingEnabled            *bool    `json:"api_self_account_editing_enabled,omitempty"`
+	APISelfAccountDeletionEnabled           *bool    `json:"api_self_account_deletion_enabled,omitempty"`
+	APISelfIDPLinksManagementEnabled        *bool    `json:"api_self_idplinks_management_enabled,omitempty"`
+	APISelfInitialPasswordDefinitionAllowed *bool    `json:"api_self_initial_password_definition_allowed,omitempty"`
+	ShowAuthenticatorsTab                   *bool    `json:"show_authenticators_tab,omitempty"`
+	ShowPasswordTab                         *bool    `json:"show_password_tab,omitempty"`
+	ShowProfileTab                          *bool    `json:"show_profile_tab,omitempty"`
+	ShowMailEditing                         *bool    `json:"show_mail_editing,omitempty"`
+	ShowAccountDeletionButton               *bool    `json:"show_account_deletion_button,omitempty"`
+	ShowIDPLinksTab                         *bool    `json:"show_idplinks_tab,omitempty"`
+	SelfServiceDefaultTab                   *string  `json:"self_service_default_tab,omitempty"`
+	AllowedBackURL                          *string  `json:"allowed_back_url,omitempty"` // DEPRECATED
+	AllowedBackURLs                         []string `json:"allowed_back_urls,omitempty"`
 
 	RedirectCancelledRegistrationURL  *string   `json:"redirect_cancelled_registration_url,omitempty"`
 	RedirectSuccessfulRegistrationURL *string   `json:"redirect_successful_registration_url,omitempty"`
